13156e67b629d8262c82db0e02977655f9b1d6c2
Uri <oriolasensioserra@cev.com>
2025-01-17 14:12:51 +0100

    canvas
    canvas

--- Assets/PACKAGE/Scripts/SCR_Pickup.cs
+++ Assets/PACKAGE/Scripts/SCR_Pickup.cs
@@ -6,59 +6,48 @@ public class SCR_Pickup : MonoBehaviour
 {
     public GameObject pickup;
     public Transform mano;
-
+    public GameObject textoCanvas; // Referencia al texto en el Canvas
     private bool activo;
-
-
+    private bool objetoEnMano; // Variable para saber si el objeto está en la mano
 
     // Update is called once per frame
     void Update()
     {
         if (activo == true)
         {
-
-            if (Input.GetKeyDown(KeyCode.E))
+            if (Input.GetKeyDown(KeyCode.E) && !objetoEnMano) // Solo permitir al jugador recoger si no tiene el objeto
             {
-
                 pickup.transform.SetParent(mano);
                 pickup.transform.position = mano.position;
                 pickup.GetComponent<Rigidbody>().isKinematic = true;
-
-
+                objetoEnMano = true;
+                textoCanvas.SetActive(false); // Ocultar el texto cuando el objeto es recogido
             }
-
         }
 
-        if (Input.GetKeyDown(KeyCode.R))
-        { 
-        
+        if (Input.GetKeyDown(KeyCode.R) && objetoEnMano)
+        {
             pickup.transform.SetParent(null);
             pickup.GetComponent<Rigidbody>().isKinematic = false;
-
+            objetoEnMano = false;
         }
-
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player")
-        { 
-        
-        activo = true;
-
+        if (other.tag == "Player" && !objetoEnMano) // Solo activar el texto si no tienes el objeto en la mano
+        {
+            activo = true;
+            textoCanvas.SetActive(true); // Activar el texto en el Canvas cuando el jugador entre al collider
         }
     }
-
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.tag == "Player")
+        if (other.tag == "Player" && !objetoEnMano) // Solo desactivar el texto si no tienes el objeto en la mano
         {
-
             activo = false;
-
+            textoCanvas.SetActive(false); // Desactivar el texto cuando el jugador salga del collider
         }
     }
-
-
 }
