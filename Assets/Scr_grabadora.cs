7c03e13f32a1d9ec5cf6bc94db37a3a96cd90716
Roger-Tost <rogertostferreira@cev.com>
2025-05-22 14:22:21 +0200

    waka wakie acabado

--- Assets/Scr_grabadora.cs
+++ Assets/Scr_grabadora.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Windows;
 
 public class Scr_grabadora : MonoBehaviour
 {
     public GameObject interactionPromptUI;
-    [SerializeField] string texttoshow;
+    public string[] TextosParaMostrar;
+    public AudioSource[] abuela;
+    int textoActual;
     [SerializeField] TextMeshProUGUI textUI;
     [SerializeField] float time;
+    public bool coldown = false;
     bool activo;
 
     // Start is called before the first frame update
@@ -21,9 +25,54 @@ public class Scr_grabadora : MonoBehaviour
 
     private void Update()
     {
-        if (UnityEngine.Input.GetKeyDown(KeyCode.E) && activo) 
+        if (UnityEngine.Input.GetKeyDown(KeyCode.E) && activo && !coldown) 
         {
-            StartCoroutine(nameof(ShowText));
+            if (textoActual < TextosParaMostrar.Length)
+            {
+                //aqui va el audio de enceder
+                StartCoroutine(ShowText(TextosParaMostrar[textoActual]));
+
+                switch (textoActual) 
+                {
+                    case 0: abuela[0].Play(); Debug.Log("audio 0"); break;
+
+                    case 1: abuela[1].Play(); Debug.Log("audio 1"); break;
+
+                    case 2: abuela[2].Play(); Debug.Log("audio 2"); break;
+
+                    case 3: abuela[3].Play(); Debug.Log("audio 3"); break;
+
+                    case 4: abuela[4].Play(); Debug.Log("audio 4"); break;
+
+                }
+
+                textoActual++;
+
+            }
+
+            else
+            {
+   
+                textoActual = 0;
+                StartCoroutine(ShowText(TextosParaMostrar[textoActual]));
+                
+                switch (textoActual) 
+                {
+                    case 0: abuela[0].Play(); Debug.Log("audio 0"); break;
+
+                    case 1: abuela[1].Play(); Debug.Log("audio 1"); break;
+
+                    case 2: abuela[2].Play(); Debug.Log("audio 2"); break;
+
+                    case 3: abuela[3].Play(); Debug.Log("audio 3"); break;
+
+                    case 4: abuela[4].Play(); Debug.Log("audio 4"); break;
+
+                }
+
+                textoActual++;
+
+            }
         }
 
     }
@@ -50,22 +99,24 @@ public class Scr_grabadora : MonoBehaviour
 
     }
 
-    IEnumerator ShowText() 
+    IEnumerator ShowText(string texto) 
     {
-        textUI.text = texttoshow;
+        textUI.text = texto;
         textUI.maxVisibleCharacters = 0;
 
-        foreach (char c in texttoshow) 
+        foreach (char c in texto) 
         {
+            coldown = true;
             textUI.maxVisibleCharacters++;
             yield return new WaitForSeconds(time);
+            
         }
+        Debug.Log("finished");
+        coldown = false;
 
+        foreach (var audio in abuela)
+        {
+            audio.Stop();
+        }
     }
-
 }
-
-
-
-
-
